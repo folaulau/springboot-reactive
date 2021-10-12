a04197466dab6eefff636bcf1d2c68e6bfc6c16d
Folau Kaveinga <folaukaveinga@gmail.com>
2021-10-12 12:34:59 -0600

    setup

---
+++ src/test/java/com/lovemesomecoding/SpringbootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
